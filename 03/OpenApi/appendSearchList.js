65b3dcbcfebddd35fd5d00c0f69a97cbaf078312
Dongha Kim <ghost9087@gmail.com>
2015-04-04 20:55:56 +0900

    add openapi search page

---
+++ 03/OpenApi/appendSearchList.js
@@ -0,0 +1,23 @@
+/**
+ * Created by ghost9087 on 15. 4. 2..
+ */
+
+var search_tmpl = document.getElementById("search_tmpl").innerHTML;
+
+function loadJSON(object){
+    var list_containor = document.getElementById('search_list');
+    appendList(list_containor, object.channel.item);
+
+}
+
+function appendList(list_containor, item) {
+    for(var index in item){
+        var list_item = search_tmpl.replace('{link}', item[index].link);
+        list_item = list_item.replace('{title}', item[index].title);
+
+        list_containor.innerHTML += list_item;
+    }
+}
+function clearList(list_containor) {
+    list_containor.innerHTML = '';
+}
---
+++ 03/OpenApi/loadPage.js
@@ -0,0 +1,40 @@
+/**
+ * Created by ghost9087 on 15. 4. 2..
+ */
+setButton();
+var currentPage = 1;
+var searchWord = '';
+function loadScript(searchword, pageNumber)
+{
+    currentPage = pageNumber;
+    var head = document.getElementsByTagName("head")[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://apis.daum.net/search/web?apikey=a90f077791b6169ebbf969b16fe594ae&q='+searchword+'&output=json&callback=loadJSON&pageno='+pageNumber;
+    head.appendChild(script);
+}
+function setButton(){
+    setSearchButton();
+    setMoreButton();
+}
+function getSearchWord() {
+    var input = document.getElementById('search_word');
+    return input.value;
+}
+function setSearchButton() {
+    var next = document.getElementById("btn_search");
+    next.addEventListener("click", function(ev){
+        var list_containor = document.getElementById('search_list');
+        list_containor.innerHTML = '';
+
+        searchWord = getSearchWord();
+
+        loadScript(searchWord, 1);
+    });
+}
+function setMoreButton() {
+    var more = document.getElementById('more');
+    more.addEventListener('click', function(ev){
+        loadScript(searchWord, currentPage+1);
+    });
+}
